566814023dfca5b30304858a1a750976e88e596b
siyucoco <sic323@lehigh.edu>
2022-07-12 16:38:52 -0400

    changed dT/dt equation

--- py/helper.py
+++ py/helper.py
@@ -17,25 +17,27 @@ import numpy as np
 
 # --------------------- Static Parameters    --------------------- #
 
-b0 = 9.3 * math.exp(-10)  # 93      unit : 1/bars
-deltH_0 = 95.3  # j/mol 
-Tw = -5.0  # room temperature
-T0 = 353.15  # temeperature in kelvin
-t_h0 = .37  # heterogeneity constant
+b0 = 93 * (10**-5)  # 93      unit : 1/bars
+deltH_0 = 95300  #               unit: j/mol 
+Tw = 278.0  #  room temperature  unit: kelvin
+T0 = 353.15  # temeperature in   unit: kelvin
+t_h0 = .37  # heterogeneity constant 
 apha = 0.33
 chi = 0.0
-q_s0 = 3.40 # qs_var = q_s0 = 3.4 due to chi = 0
-R = 8.314* (10**-5) # Universal gas constant - J/mol-K
+q_s0 = 3.40 # qs_var = q_s0 = 3.4 due to chi = 0 mol/kg
+# R = 8.314* (10**3) # Universal gas constant - LPa/molK
 Rg = .0821 # Universal gas constant in l-atm/mol/K
-kT0 = 3.5 * (10 ** -2)  # used to compute kT for r_CO2... in mol/Kg-bar-sec
+kT0 = 3.5 * (10 ** -2)  # used to compute kT for r_CO2... in mol/Kg-pa-sec
 EaCO2 = 15200 # activation for KT -- J/mol
 ps = 880.0
-deltH_co2 = 75000.0  # calculate temeprature change
+deltH_co2 = 75000.0  # calculate temeprature change   unit: jol/mol
+
+R_constant = 8.314 # jol/kelvin-mol
 
 # ------------------ For Equation : Enegergy Ballance  -------------- #
 pg = 1.87  # 
 h = 13.8
-Cp_g = 846  # J/kgK
+Cp_g = 846.0  # J/kgK
 Cp_s = 1500.0  # J/kgK
 
 # ------------------ ODE Repetitive Shortcut -------------- #
@@ -66,11 +68,15 @@ def masss_balan2(episl_r, ps):
     # print(mass_balan2)
     return (mass_balan2)
 
+def cube(x):
+    if 0<=x: return x**(1./3.)
+    return -(-x)**(1./3.)
+
 # ------------------ Equastions to calculate Rco2 -------------- #
 def b(T): # will be called inside deriv
-    # print(T)
-    b = b0 *math.exp(((deltH_0 / (R * T0)) * (T0 / T - 1)))
-    print(f'b, {b}')
+    # print(f"T", {T})
+    b = b0 *(math.exp(((deltH_0 / (R_constant * T0)) * (T0 / T - 1))))
+    # print(f'b, {b}')
     return b
 
 def t_h(T): # will be call inside  deriv
@@ -81,20 +87,10 @@ def t_h(T): # will be call inside  deriv
     # print(t_h)
     return (t_h)
 
-def q_s(T):
-    q_s = q_s0 *math.exp((chi * (1 - T / T0)))
-    # print(q_s)
-    return (q_s)
-    # q_s = math(q_s0, (chi * (1 - T / T0)))
-   
-    # return (q_s)
-
-
 # Calculate rco2_n (not ode)
 def R_co2(T, c_co2, q, b_var, t_var):
     # b_var = b(T)
     # t_var = t_h(T)
-    # # print(t_var)
     # print(a)
     # print(q)
     # print(c_co2)
@@ -109,17 +105,27 @@ def R_co2(T, c_co2, q, b_var, t_var):
     # rco2_term4 = rco2_term3 - q / (b_var * q_s0)
     # rco2_term5 = kT * rco2_term4
     # term = kT * (R * T * c_co2 * ((1 - ((q / q_s0) ** (t_var))) ** (1 / t_var)) - q / (b_var * q_s0))
-    # print(f'term, {term}')
-    # print(f"rco2_term1, {rco2_term1}.  ")
-    # print(f"rco2_term2, {rco2_term2}. ")
-    # print(f"(rco2_term3), {rco2_term3}. ")
-    # print(f"(rco2_term4), {rco2_term4}. ")
-    # Note that R_CO2 is in mol/Kg-s... kT is in mol/Kg-bar-sec and so R*T*CO2 needs to be in units of pressure (bar). Therefore, R should be in lit-atm/mol-K, T in K, and C_CO2 should be in mol/lit 
-    r_co2 = kT0 * math.exp(-EaCO2/R*T) * (Rg * T * c_co2 * ((1 - ((q / q_s0) ** (t_var))) ** (1 / t_var)) - q / (b_var * q_s0))
-    # print(f"r_co2",{r_co2})
-
+    # Note that R_CO2 is in mol/Kg-s... kT is in mol/Kg-bar-sec and so R*T*CO2 needs to be in units of pressure (bar). Therefore, R should be in lit-atm/mol-K, T in K, and C_CO2 should be in mol/lit^3
+    # print(f"T", {T})
+    kn = kT0 * ( math.exp(( (-EaCO2) / (R_constant*T) )))
+    # print(f"T in rco2", {T})
+    # print(f"kn",{kn})
+    rco2_1 = Rg * T * c_co2 
+    # print(f"Rg * T * c_co2", {rco2_1})
+    # print(f"th", {t_var})
+    rco2_2 = ((1 - ((q / q_s0) ** (t_var))) ** (1 / t_var))
+    # print(f"rco2_2", {rco2_2})
+    rco2_3 = q / (b_var * q_s0)
+    # print(f"rco2_3", {rco2_3})
+    # r_co2_part1 = rco2_1    
+    rco2 = kn * (rco2_1 * rco2_2 - rco2_3)
+    # print(f"rco2", {rco2})
+    r_co2_part1 =(Rg * T * c_co2 * ((1 - ((q / q_s0) ** (t_var))) ** (1 / t_var)) - q / (b_var * q_s0))
+    # print(f"rco2_part1",{r_co2_part1})
+    # r_co2 = kn *r_co2_part1
+    # print(f'r_co2, {r_co2}')
     # print(f"b_var * qs_var, {rco2_term5}.")
-    return r_co2
+    return rco2
 
 """
     Defines the differential equations for odes of DAC
@@ -133,20 +139,21 @@ def R_co2(T, c_co2, q, b_var, t_var):
 """
 def deriv(t, y, params):
     T_n, co2_n, q_n, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12 = y # the rest of 12 vars a_n are not used, only for the success of solve_ivp   , 
-    V, r, T, c_co2_0, episl_r, v0 = params
+    V, T, c_co2_0, episl_r, volumetric_flow = params
     ###############   -----  Parameters depend on input  -----  ###############
+    r = cube(V/(20*math.pi))
+    v0 = volumetric_flow / (math.pi *r*r )
     L = V / (math.pi * (r ** 2))
     deltZ = L / 5.0  # 5 boxes in total
-    a_s = deltZ / r
+    a_s = 2 / r
     theta = (1 - episl_r) * ps * Cp_s + episl_r * pg * Cp_g
 
-    b_var = b(T)
     t_var = t_h(T)
-   # T_n, co2_n, q_n, T_n2, co2_n2, q_n2, T_n3, co2_n3, q_n3, T_n4, co2_n4, q_n4, T_n5, co2_n5, q_n5 == y
-    # rco2_ first, rate of generation
+    # print(t_var)
+    b_var = b(T)
     T1 = -ener_balan(v0, theta, deltZ) * T_n + ener_balan(v0, theta, deltZ) * T0 + ener_balan2(episl_r) * (
         R_co2(T_n, co2_n, q_n, b_var, t_var)) + ener_balan3(a_s, T_n)
-    # print(T1)
+  
     co2_1dot = -mass_balan(v0, episl_r, deltZ) * co2_n + mass_balan(v0, episl_r, deltZ) * c_co2_0 - (
         R_co2(T_n, co2_n, q_n,  b_var, t_var)) * masss_balan2(episl_r, ps)
     q1dot = R_co2(T_n, co2_n, q_n,  b_var, t_var)
@@ -182,13 +189,15 @@ def deriv(t, y, params):
 
 def deriv1(t, y, params):
     T_n, co2_n, q_n, T_n2, co2_n2, q_n2,T_n3, co2_n3, q_n3, T_n4, co2_n4, q_n4,T_n5, co2_n5, q_n5 = y # the rest of 12 vars a_n are not used, only for the success of solve_ivp
-    V, r, T, c_co2_0, episl_r, v0 = params
+    V, T, c_co2_0, episl_r, volumetric_flow = params
+
     ###############   -----  Parameters depend on input  -----  ###############
+    r = cube(V/(20*math.pi))
+    v0 = volumetric_flow / (math.pi *r*r )
     L = V / (math.pi * (r ** 2))
     deltZ = L / 5.0  # 5 boxes in total
     a_s = 2 / r
     theta = (1 - episl_r) * ps * Cp_s + episl_r * pg * Cp_g
-
     
     t_var = t_h(T)
     # print(t_var)
@@ -196,31 +205,35 @@ def deriv1(t, y, params):
    # T_n, co2_n, q_n, T_n2, co2_n2, q_n2, T_n3, co2_n3, q_n3, T_n4, co2_n4, q_n4, T_n5, co2_n5, q_n5 == y
     # rco2_ first, rate of generation
     T1 = -ener_balan(v0, theta, deltZ) * T_n + ener_balan(v0, theta, deltZ) * T0 + ener_balan2(episl_r) * (
-        R_co2(T_n, co2_n, q_n,  b_var, t_var)) + ener_balan3(a_s, T_n)
+        R_co2(T_n, co2_n, q_n,  b_var, t_var))/theta + ener_balan3(a_s, T_n)/theta
+    # print(f"T1", {T1})
     co2_1dot = -mass_balan(v0, episl_r, deltZ) * co2_n + mass_balan(v0, episl_r, deltZ) * c_co2_0 - (
         R_co2(T_n, co2_n, q_n,  b_var, t_var)) * masss_balan2(episl_r, ps)
     q1dot = R_co2(T_n, co2_n, q_n,  b_var, t_var)
-
+    # print(f"energy balance in T1", {ener_balan(v0, theta, deltZ)})
+    
     T2 = -ener_balan(v0, theta, deltZ) * T_n2 + ener_balan(v0, theta, deltZ) * T1 + ener_balan2(episl_r) * (
-        R_co2(T_n2, co2_n2, q_n2,  b_var, t_var)) + ener_balan3(a_s,  T_n2)
+        R_co2(T_n2, co2_n2, q_n2,  b_var, t_var))/theta + ener_balan3(a_s,  T_n2)/theta
+    # print(f"T2", {T2})
     co2_2dot = -mass_balan(v0, episl_r, deltZ) * co2_n2 + mass_balan(v0, episl_r, deltZ) * co2_1dot - (
         R_co2(T_n2, co2_n2, q_n2,  b_var, t_var)) * masss_balan2(episl_r, ps)
     q2dot = R_co2(T_n2, co2_n2, q_n2,  b_var, t_var)
+    # print(f"energy balance in T1", {ener_balan(v0, theta, deltZ)})
 
     T3 = -ener_balan(v0, theta, deltZ) * T_n3 + ener_balan(v0, theta, deltZ) * T2 + ener_balan2(episl_r) * (
-        R_co2(T_n3, co2_n3, q_n3,  b_var, t_var)) + ener_balan3(a_s, T_n3)
+        R_co2(T_n3, co2_n3, q_n3,  b_var, t_var))/theta + ener_balan3(a_s, T_n3)/theta
     co2_3dot = -mass_balan(v0, episl_r, deltZ) * co2_n3 + mass_balan(v0, episl_r, deltZ) * co2_2dot - (
         R_co2(T_n3, co2_n3, q_n3,  b_var, t_var)) * masss_balan2(episl_r, ps)
     q3dot = R_co2(T_n3, co2_n3, q_n3,  b_var, t_var)
 
     T4 = -ener_balan(v0, theta, deltZ) * T_n4 + ener_balan(v0, theta, deltZ) * T3 + ener_balan2(episl_r) * (
-        R_co2(T_n4, co2_n4, q_n4,  b_var, t_var)) + ener_balan3(a_s,  T_n4)
+        R_co2(T_n4, co2_n4, q_n4,  b_var, t_var))/theta + ener_balan3(a_s,  T_n4)/theta
     co2_4dot = -mass_balan(v0, episl_r, deltZ) * co2_n4 + mass_balan(v0, episl_r, deltZ) * co2_3dot - (
         R_co2(T_n4, co2_n4, q_n4,  b_var, t_var)) * masss_balan2(episl_r, ps)
     q4dot = R_co2(T_n4, co2_n4, q_n4,  b_var, t_var)
 
     T5 = -ener_balan(v0, theta, deltZ) * T_n5 + ener_balan(v0, theta, deltZ) * T4 + ener_balan2(episl_r) * (
-        R_co2(T_n5, co2_n5, q_n5,  b_var, t_var)) + ener_balan3(a_s,  T_n5)
+        R_co2(T_n5, co2_n5, q_n5,  b_var, t_var))/theta + ener_balan3(a_s,  T_n5)/theta
     co2_5dot = -mass_balan(v0, episl_r, deltZ) * co2_n5 + mass_balan(v0, episl_r, deltZ) * co2_4dot - (
         R_co2(T_n5, co2_n5, q_n5,  b_var, t_var)) * masss_balan2(episl_r, ps)
     q5dot = R_co2(T_n5, co2_n5, q_n5,  b_var, t_var)
@@ -230,23 +243,23 @@ def deriv1(t, y, params):
     return [T1, co2_1dot, q1dot, T2, co2_2dot, q2dot, T3, co2_3dot, q3dot, T4, co2_4dot, q4dot, T5, co2_5dot, q5dot]
 
 # ------------------ User generated - Slider initial value -------------- #
-V = 100.0  # volume
-r = 5.0 ### no need 
+V = 200.0  # volume
 T = 293.0 # +273 ambrient temperature
-c_co2_0 = 5.0*(10**2) # mol/m^3 
+c_co2_0 = .016349 # mol/m^3    
 episl_r = 0.3  # void
-v0 = 2.0  # initial vilocity
+volumetric_flow = 5 # m^3/s
+
 # air humidity 
-# no radius and length, nonly need V, july 6th
+# no radius and length, nonly nr *reed V, july 6th
 
 # ------------------ Initial Conditions to set up solve_ivp -------------- #
-t0, tf = 0.0, 10800.0 # 3hrs
+t0, tf = 0.0, 1000.0 # 3hrs
 
 # init_cond = [20.000, 0.000, 0.000]
-init_cond = [20.000, 0.000, 0.000,20.000, 0.000, 0.000, 20.000, 0.000, 0.000, 20.000, 0.000, 0.000, 20.000, 0.000, 0.000]
+init_cond = [293.000, 0.000, 0.000,293.000, 0.000, 0.000, 293.000, 0.000, 0.000, 293.000, 0.000, 0.000, 293.000, 0.000, 0.000]
 # ,20.000, 0.000, 0.000,20.000, 0.000, 0.000,20.000, 0.000, 0.000,20.000, 0.000, 0.000
-params = [V, r, T, c_co2_0, episl_r, v0]
-soln = solve_ivp(deriv1, (t0, tf), init_cond, args=(params,))  # init_cond = (T, c_co2_0, q0)
+params = [V, T, c_co2_0, episl_r, volumetric_flow]
+soln = solve_ivp(deriv1, (t0, tf), init_cond, args=(params,), method='RK45')  # init_cond = (T, c_co2_0, q0)
 
 print(soln)
 # Equation 3
@@ -256,10 +269,15 @@ print(soln)
 # temperature should be flat, c02 sould drop, q should increase, 
 # need to know when it reaches to L, then stop the whole operation
 # time when it reaches the breakthrough and desorption 
-T1, T2, T3, T4, T5 = soln.y[0], soln.y[3], soln.y[6], soln.y[9], soln.y[12]
-co2_1dot, co2_2dot, co2_3dot, co2_4dot, co2_5dot = soln.y[1], soln.y[4], soln.y[7], soln.y[10], soln.y[13]
-q1dot, q2dot, q3dot, q4dot, q5dot = soln.y[2], soln.y[5], soln.y[8], soln.y[11], soln.y[14], 
 
+# T1, T2, T3, T4, T5 = soln.y[0], soln.y[3], soln.y[6], soln.y[9], soln.y[12]
+# co2_1dot, co2_2dot, co2_3dot, co2_4dot, co2_5dot = soln.y[1], soln.y[4], soln.y[7], soln.y[10], soln.y[13]
+# q1dot, q2dot, q3dot, q4dot, q5dot = soln.y[2], soln.y[5], soln.y[8], soln.y[11], soln.y[14], 
+
+# print(co2_1dot )
+# print("\n")
+# print( co2_2dot)
+# print( co2_3dot, co2_4dot, co2_5dot)
 # temperature_name = ['T1', 'T2', 'T3', 'T4', 'T5']
 # temperature_colors = ['lightsteelblue','deepskyblue', 'dodgerblue', 'mediumblue', 'navy']
 # # print(temperature_name)
@@ -384,6 +402,7 @@ q1dot, q2dot, q3dot, q4dot, q5dot = soln.y[2], soln.y[5], soln.y[8], soln.y[11],
 # curdoc().title = "Direct Air Capture"
 # # tabs = Tabs(tabs=[ tab1, tab ])
 # # show(tabs)
+
 
 
 
